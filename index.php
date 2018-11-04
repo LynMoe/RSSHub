64517cd9cf8bb8a551d9f86be4e7a2c7293692e1
XiaoLin <i@cgl.li>
2018-11-04 15:49:59 +0800

    Add error pages with Kugelblitz's help

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
  * Time: 5:55 PM
  */
 
-if (!isset($_GET['s']))
-    die('非法操作');
-
-if (!is_dir(__DIR__ . '/vendor') || !file_exists(__DIR__ . '.env'))
-    require_once __DIR__ . '/core/start.php';
+if (is_dir(__DIR__ . '/vendor') && file_exists(__DIR__ . '.env'))
+    if (!isset($_GET['s']))
+        die(file_get_contents(__DIR__ . '/view/homepage.html'));
+    else
+        require_once __DIR__ . '/core/start.php';
 else
-    die('请先完成安装.');
+    die(file_get_contents(__DIR__ . '/view/notinstalled.html'));
